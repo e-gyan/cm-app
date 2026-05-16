07a8592b1ddb5eadf231bc7d15c2bc57cc1c5e54
Emmanuel Gyan <calculusgyan@gmail.com>
2026-05-16 21:33:26 +0000

    feat: Update deployment configuration
    Adjusts the GitHub Pages deployment workflow and Vite base URL for correct deployment to the `cm-app` repository. Also updates a dummy API key in a test file.

--- test-jsonbin.cjs
+++ test-jsonbin.cjs
@@ -8,7 +8,7 @@ const req = https.request('https://api.jsonbin.io/v3/b', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'X-Master-Key': '$2a$10$K5M6PsdqUpAmMJHp06t1PeEK2tabwlgLoFMHLo/yEWV5ndxGCMcRu0' // dummy
+    'X-Master-Key': '$2a$10$wT/3G.A8s/sS1rJ7YwT7V.N6/vV2/f0V6/vV2/f0V6/vV2/f0' // dummy
   }
 }, res => {
   let chunks = '';
